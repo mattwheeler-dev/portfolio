ebb6e5138d929394fbae89efb2c3f610b0ac510b
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2024-06-26 20:42:34 -0700

    Init Commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// CURSOR FOLLOWING CIRCLE
+const moveCircle = (e) => {
+	const circle = document.querySelector("#cursor-circle");
+	const posY = e.pageY;
+	const posX = e.pageX;
+
+	circle.style.top = `${posY - 20}px`;
+	circle.style.left = `${posX - 20}px`;
+};
+
+window.addEventListener("mousemove", moveCircle);
+
+// CHANGE ACTIVE NAV ITEM BASED ON VIEW POSITION
+// SET NAV ITEM ACTIVE BASED ON PAGE POSITION
+const activeLink = () => {
+	const sections = document.querySelectorAll("section");
+	const navLinks = document.querySelectorAll("#nav-links a");
+	let index = sections.length;
+
+	while (--index && window.scrollY + 250 < sections[index].offsetTop) {}
+
+	navLinks.forEach((link) => link.classList.remove("active"));
+	navLinks[index].classList.add("active");
+};
+
+activeLink();
+window.addEventListener("scroll", activeLink);
+
+// INTERACTIVE WORD INTERACTION
+const interactive = document.querySelector("#interactive");
+
+interactive.addEventListener("click", () => {
+	interactive.classList.toggle("toggle-inter");
+});
